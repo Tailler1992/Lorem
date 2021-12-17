9c6d6f089d065eb242caa444738902c3914d7bea
Aleksandr Peretertov <home.mail.tailler@gmail.com>
2021-12-17 17:20:24 +0500

    Сверстан блок questions

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,24 +1,22 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    //direction: 'vertical',
-    loop: true,
+  // Optional parameters
+  //direction: 'vertical',
+  loop: true,
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-
-
-  });
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
